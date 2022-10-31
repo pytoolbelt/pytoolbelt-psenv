ec413823f95f8cd170f9af9067f52e91e631edde
Jesse Maitland <jesse.maitland@heyjobs.de>
2022-10-31 09:50:33 +0100

    add readme and help

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# psenv
-### cli tool for managing local environment and aws parameter store
+# psenv -- (Parameter Store Environment)
+### a cli tool for managing environments stored in the AWS parameter store
+
+
+## Getting Started
+``pip install psenv``
+
+or better yet, install as a standalone application using pipx
+
+``pipx install psenv`` --> This is the best option
+
+
+### Create a config file
+at the terminal run
+```shell
+psenv init
+```
+
+This will create a directory at ``~/.psenv`` and a config file at `~/.psenv/psenv.yml`
+this file is where you will define your environments.
+
+### Example psenv.yml
+```yaml
+environments:
+  some_meaningful_name:
+    path: /the/path/used/in-the/aws/parameter-store
+    env: /path/to/local/env/file/.env
+
+```
+
+You can now run
+
+```shell
+psenv show
+```
+
+You should see a list of all available environments
+
+
+## Fetch an Environment from the AWS Parameter Store
+
+This command will fetch all the parameters on the path configured in the ``path`` section of the `psenv.yml` file
+```
+psenv fetch --env some_meaningful_name
+```
+
+## Push and Environement to the AWS Parameter Store
+Sometimes you need to set up new environments. This is easy with `psenv`
+add an entry in `psenv.yml`
+
+```yaml
+environments:
+  new_env:
+    path: /some/parameter-store/path
+    env: /path/to/.env
+```
+
+populate the .env file with the secrets and parameters you need for your environment and then run
+``psenv push --env new_env``
+
+
+## Credential Injection (Currently just AWS Supported)
+Sometimes you might want to copy some environment variables from your local env to a .env file.
+for example when you use temp AWS credentials and need to load them into more than one project / environment. To do this, get your creds however you normally would
+and then
+
+``psenv inject aws --env some_env``
+
+all your aws credentials will be injected to your .env file! Like Magic!
--- psenv/cli/__init__.py
+++ psenv/cli/__init__.py
@@ -9,21 +9,21 @@ def parse_args():
     sub_parsers.required = True
 
     version_parser = sub_parsers.add_parser("version")
-    version_parser.set_defaults(func=version.version_entrypoint)
+    version_parser.set_defaults(func=version.version_entrypoint, help="display the current version of psenv")
 
     init_parser = sub_parsers.add_parser("init")
-    init_parser.set_defaults(func=init.init_entrypoint)
+    init_parser.set_defaults(func=init.init_entrypoint, help="run this command once to create the ~/.psenv directory with config.")
 
     show_parser = sub_parsers.add_parser("show")
-    show_parser.set_defaults(func=show.show_entrypoint)
+    show_parser.set_defaults(func=show.show_entrypoint, help="print all the configured environments in ~/.psenv/psenv.yml to the terminal")
 
     fetch_parser = sub_parsers.add_parser("fetch")
     fetch_parser.add_argument("-m", "--method", choices=["overwrite", "update"], default="overwrite")
-    fetch_parser.set_defaults(func=fetch.fetch_entrypoint)
+    fetch_parser.set_defaults(func=fetch.fetch_entrypoint, help="pull parameters from the path in parameter store and populate an .env file.")
 
     push_parser = sub_parsers.add_parser("push")
     push_parser.set_defaults(func=push.push_entrypoint)
-    push_parser.add_argument("-o", "--overwrite", default=False, action="store_true")
+    push_parser.add_argument("-o", "--overwrite", default=False, action="store_true", help="overwrite the existing parameter store value.")
 
     env_parser = sub_parsers.add_parser("env")
     env_subparser = env_parser.add_subparsers()
@@ -37,14 +37,14 @@ def parse_args():
 
     inject_parser = sub_parsers.add_parser("inject")
     inject_parser.set_defaults(func=inject.inject_entrypoint)
-    inject_parser.add_argument("prefix", choices=["aws"])
+    inject_parser.add_argument("prefix", choices=["aws"], help="copy from local environment to .env file")
 
     # adding the environment flag to necessary commands
     for p in fetch_parser, push_parser, env_new_parser, env_destroy_parser, inject_parser:
-        p.add_argument("-e", "--env", required=True, action=validation.ValidateEnvironmentName)
+        p.add_argument("-e", "--env", required=True, action=validation.ValidateEnvironmentName, help="name of the configured environment")
 
     # adding the path flag to necessary commands
     for p in env_new_parser, env_destroy_parser:
-        p.add_argument("-p", "--path", action=validation.ValidatePathName)
+        p.add_argument("-p", "--path", action=validation.ValidatePathName, help="variable path in the parameter store")
 
     return parser.parse_args()
