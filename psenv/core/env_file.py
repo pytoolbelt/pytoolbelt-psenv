861310a4437df622904daacb5396cef8648f4c70
Jesse Maitland <jesse.maitland@heyjobs.de>
2022-11-11 06:11:01 +0100

    wip

--- psenv/core/env_file.py
+++ psenv/core/env_file.py
@@ -1,13 +1,28 @@
+from io import StringIO
 from pathlib import Path
 from typing import Dict, List
 from dotenv import dotenv_values
+
+PRIVATE_MARKER = "#<private>"
 
 
 class EnvFile:
     def __init__(self, path: Path) -> None:
         self.path = path
 
+        # set up main and private content
+        content = self.path.read_text().partition(PRIVATE_MARKER)
+        self.main_content = self._parse_values_to_dict(content[0])
+        self.private_content = self._parse_values_to_dict(content[2])
+
         self._methods = {"overwrite": self._overwrite_env, "update": self._update_env}
+
+    @staticmethod
+    def _parse_values_to_dict(content: str) -> Dict[str, str]:
+        with StringIO() as buffer:
+            buffer.write(content)
+            buffer.seek(0)
+            return dotenv_values(stream=buffer)
 
     def write_params_to_env(self, params: Dict[str, str], method: str = "overwrite") -> None:
 
@@ -37,6 +52,7 @@ class EnvFile:
                 value = params[key]
                 line = f"{key}={value}"
                 env.write(f"{line}\n")
+            env.write(PRIVATE_MARKER)
 
     def _update_env(self, params: Dict[str, str]) -> None:
         current_values = dotenv_values(self.path)
