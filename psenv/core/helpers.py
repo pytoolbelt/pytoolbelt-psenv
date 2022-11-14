efaf0b92ba069985a5a33c135c329bcc4b2f2741
Jesse Maitland <jesse.maitland@heyjobs.de>
2022-11-14 07:18:31 +0100

    feat: add prefix flag

--- psenv/core/helpers.py
+++ psenv/core/helpers.py
@@ -4,6 +4,7 @@ from typing import Dict
 
 def get_environment_variables(prefix: str) -> Dict[str, str]:
     retrieved = {}
+    prefix = prefix.upper()
     for key, value in os.environ.items():
         if key.startswith(prefix):
             retrieved[key] = value
