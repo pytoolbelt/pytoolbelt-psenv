726f7c8d8b0357bcca738663d484853d3169ce9b
Jesse Maitland <jesse.maitland@heyjobs.de>
2022-11-15 09:09:37 +0100

    feat: add cli help

--- psenv/cli/__init__.py
+++ psenv/cli/__init__.py
@@ -8,36 +8,30 @@ def parse_args():
     sub_parsers = parser.add_subparsers(dest="command")
     sub_parsers.required = True
 
-    version_parser = sub_parsers.add_parser("version")
-    version_parser.set_defaults(func=version.version_entrypoint, help="display the current version of psenv")
+    version_parser = sub_parsers.add_parser("version", help="Display the current version of psenv")
+    version_parser.set_defaults(func=version.version_entrypoint)
 
-    init_parser = sub_parsers.add_parser("init")
-    init_parser.set_defaults(
-        func=init.init_entrypoint, help="run this command once to create the ~/.psenv directory with config."
-    )
+    init_parser = sub_parsers.add_parser("init", help="Init psenv")
+    init_parser.set_defaults(func=init.init_entrypoint)
 
-    show_parser = sub_parsers.add_parser("show")
-    show_parser.set_defaults(
-        func=show.show_entrypoint, help="print all the configured environments in ~/.psenv/psenv.yml to the terminal"
-    )
+    show_parser = sub_parsers.add_parser("show", help="Print configured environments to the terminal")
+    show_parser.set_defaults(func=show.show_entrypoint)
 
-    fetch_parser = sub_parsers.add_parser("fetch")
+    fetch_parser = sub_parsers.add_parser("fetch", help="Fetch parameters from the AWS Parameter Store")
     fetch_parser.add_argument("-m", "--method", choices=["overwrite", "update"], default="update")
-    fetch_parser.set_defaults(
-        func=fetch.fetch_entrypoint, help="pull parameters from the path in parameter store and populate an .env file."
-    )
+    fetch_parser.set_defaults(func=fetch.fetch_entrypoint)
 
-    push_parser = sub_parsers.add_parser("push")
+    push_parser = sub_parsers.add_parser("push", help="Push parameters to the AWS Parameter Store")
     push_parser.set_defaults(func=push.push_entrypoint)
     push_parser.add_argument(
-        "-o", "--overwrite", default=False, action="store_true", help="overwrite the existing parameter store value."
+        "-o", "--overwrite", default=False, action="store_true", help="overwrite existing parameter store values"
     )
 
-    new_parser = sub_parsers.add_parser("new")
+    new_parser = sub_parsers.add_parser("new", help="Create a new psenv environment")
     new_parser.add_argument("-f", "--file", action=validation.ValidateFileName)
     new_parser.set_defaults(func=new.new_entrypoint)
 
-    destroy_parser = sub_parsers.add_parser("destroy")
+    destroy_parser = sub_parsers.add_parser("destroy", help="NOT YET IMPLEMENTED")
     destroy_parser.set_defaults(func=destroy.destroy_entrypoint)
 
     inject_parser = sub_parsers.add_parser("inject", help="Inject variables from current session to a .env file")
