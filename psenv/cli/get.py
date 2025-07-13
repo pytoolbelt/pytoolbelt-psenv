d18f312cfab665f8b5d7a5f9c8b8b7035130cd0f
Jesse Maitland <jesse.maitland@moia.io>
2025-07-13 10:35:52 +0200

    wip

--- psenv/cli/get.py
+++ psenv/cli/get.py
@@ -5,8 +5,7 @@ from typing import Any
 import structlog
 from dotenv import load_dotenv
 
-from psenv import aws, fileio
-from psenv.models import load_config
+from psenv.context import Context
 
 logger = structlog.get_logger(__name__)
 
@@ -27,24 +26,13 @@ def configure_parser(subparser: Any) -> None:
 
 def get_parameters(cliargs: Namespace) -> int:
     logger.info("Fetching parameters from parameter store.", config=cliargs.config)
-    config = load_config(cliargs.config)
 
-    # load environment variables from .env file
-    load_dotenv(config.envfile)
-
-    # Fetch config for the specified environment
-    config_env = config.get_config_environment(cliargs.env)
-
-    # if we are not in the expected account from the parent session, raise an error
-    sts = aws.StsClient()
-    sts.raise_if_invalid_account(config_env.environment.account)
+    ctx = Context.from_cliargs(cliargs)
 
     # Fetch parameters from the parameter store
-    parameters = aws.ParameterStoreClient(parameter_path=config_env.parameter_path, kms_key=config_env.kms_key).get_parameters()
+    parameters = ctx.ps_client.get_parameters()
 
-    env_file = fileio.EnvFile(Path(config_env.environment.envfile))
-    env_file.load()
-
-    env_file.update_params(parameters, "main")
-    env_file.write_params()
+    # write them to the configured env file
+    ctx.env_file.update_params(parameters, "main")
+    ctx.env_file.write_params()
     return 0
---
+++ psenv/context.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from psenv import aws, fileio, models
+from dotenv import load_dotenv
+from argparse import Namespace
+
+class Context:
+
+    def __init__(self, env: str, config: Path) -> None:
+
+        # Load the configuration file and get the environment configuration
+        self.config = models.load_config(config)
+        self.config_env = self.config.get_config_environment(env)
+
+        # Load environment variables from the .env file specified in the config
+        load_dotenv(self.config.envfile)
+
+        # Check the sts account to ensure we are in the expected AWS account
+        self.sts = aws.StsClient()
+        self.sts.raise_if_invalid_account(self.config_env.environment.account)
+
+        # Load the environment file and its parameters we are using for this context
+        self.env_file = fileio.EnvFile(Path(self.config_env.environment.envfile))
+        self.env_file.load()
+
+        # Initialize the Parameter Store client with the parameter path and KMS key from the config
+        self.ps_client = aws.ParameterStoreClient(
+            parameter_path=self.config_env.parameter_path,
+            kms_key=self.config_env.kms_key
+        )
+
+
+    @classmethod
+    def from_cliargs(cls, cliargs: Namespace) -> 'Context':
+        """Create a Context instance from command line arguments."""
+        return cls(env=cliargs.env, config=cliargs.config)
