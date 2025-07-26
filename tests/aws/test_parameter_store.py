ebfb94d5d7b9bf5b996ddb0938d37623ca4f2490
Jesse Maitland <jesse.maitland@moia.io>
2025-07-26 13:39:32 +0200

    add more tests

---
+++ tests/aws/test_parameter_store.py
@@ -0,0 +1,96 @@
+import pytest
+from unittest.mock import MagicMock, patch
+from psenv.aws.parameter_store import ParameterStoreClient
+from psenv.error_handling.exceptions import PsenvParameterStoreError
+
+@pytest.fixture
+def mock_ssm():
+    return MagicMock()
+
+def test_init_sets_properties(mock_ssm):
+    client = ParameterStoreClient('/foo/bar', ssm_client=mock_ssm, kms_key='my-key')
+    assert client.parameter_path == '/foo/bar'
+    assert client.ssm == mock_ssm
+    assert client.kms_key == 'my-key'
+
+
+def test_kms_key_default(mock_ssm):
+    client = ParameterStoreClient('/foo/bar', ssm_client=mock_ssm)
+    assert client.kms_key == 'alias/aws/ssm'
+
+
+def test_get_params_kwargs(mock_ssm):
+    client = ParameterStoreClient('/foo/bar', ssm_client=mock_ssm)
+    kwargs = client.get_params_kwargs
+    assert kwargs['Path'] == '/foo/bar'
+    assert kwargs['Recursive'] is True
+    assert kwargs['WithDecryption'] is True
+
+
+def test_put_params_kwargs(mock_ssm):
+    client = ParameterStoreClient('/foo/bar', ssm_client=mock_ssm, kms_key='my-key')
+    kwargs = client.put_params_kwargs('FOO', 'bar', True)
+    assert kwargs['Name'] == '/foo/bar/FOO'
+    assert kwargs['Value'] == 'bar'
+    assert kwargs['Type'] == 'SecureString'
+    assert kwargs['Overwrite'] is True
+    assert kwargs['KeyId'] == 'my-key'
+
+
+def test_parse_parameter_name():
+    assert ParameterStoreClient.parse_parameter_name('/foo/bar/BAZ') == 'BAZ'
+    assert ParameterStoreClient.parse_parameter_name('BAZ') == 'BAZ'
+
+
+def test_get_parameters_success(mock_ssm):
+    paginator = MagicMock()
+    paginator.paginate.return_value = [
+        {'Parameters': [{'Name': '/foo/bar/FOO', 'Value': 'bar'}]},
+        {'Parameters': [{'Name': '/foo/bar/BAZ', 'Value': 'qux'}]}
+    ]
+    mock_ssm.get_paginator.return_value = paginator
+    client = ParameterStoreClient('/foo/bar', ssm_client=mock_ssm)
+    params = client.get_parameters()
+    assert params == {'FOO': 'bar', 'BAZ': 'qux'}
+
+
+def test_get_parameters_error(mock_ssm):
+    paginator = MagicMock()
+    paginator.paginate.side_effect = Exception('fail')
+    mock_ssm.get_paginator.return_value = paginator
+    client = ParameterStoreClient('/foo/bar', ssm_client=mock_ssm)
+    with pytest.raises(PsenvParameterStoreError):
+        client.get_parameters()
+
+
+def test_put_parameters_success(mock_ssm):
+    client = ParameterStoreClient('/foo/bar', ssm_client=mock_ssm, kms_key='k')
+    params = {'FOO': 'bar'}
+    client.put_parameters(params, overwrite=True)
+    mock_ssm.put_parameter.assert_called_once()
+    call_args = mock_ssm.put_parameter.call_args[1]
+    assert call_args['Name'] == '/foo/bar/FOO'
+    assert call_args['Value'] == 'bar'
+    assert call_args['Overwrite'] is True
+    assert call_args['KeyId'] == 'k'
+
+
+def test_put_parameters_error(mock_ssm):
+    mock_ssm.put_parameter.side_effect = Exception('fail')
+    client = ParameterStoreClient('/foo/bar', ssm_client=mock_ssm)
+    with pytest.raises(PsenvParameterStoreError):
+        client.put_parameters({'FOO': 'bar'})
+
+
+def test_delete_parameters_success(mock_ssm):
+    client = ParameterStoreClient('/foo/bar', ssm_client=mock_ssm)
+    client.delete_parameters({'FOO': 'bar'})
+    mock_ssm.delete_parameter.assert_called_once_with(Name='/foo/bar/FOO')
+
+
+def test_delete_parameters_error(mock_ssm):
+    mock_ssm.delete_parameter.side_effect = Exception('fail')
+    client = ParameterStoreClient('/foo/bar', ssm_client=mock_ssm)
+    with pytest.raises(PsenvParameterStoreError):
+        client.delete_parameters({'FOO': 'bar'})
+
---
+++ tests/aws/test_sts.py
@@ -0,0 +1,60 @@
+import pytest
+from unittest.mock import MagicMock
+from psenv.aws.sts import StsClient
+from psenv.error_handling.exceptions import PsenvInternalError, PsenvInvalidAccountError
+
+@pytest.fixture
+def mock_sts_client():
+    mock = MagicMock()
+    mock.get_caller_identity.return_value = {'Account': '123456789012', 'UserId': 'AID...', 'Arn': 'arn:aws:iam::123456789012:user/test'}
+    return mock
+
+def test_stsclient_init_uses_passed_client(mock_sts_client):
+    client = StsClient(sts_client=mock_sts_client)
+    assert client.sts == mock_sts_client
+
+def test_stsclient_init_creates_client(monkeypatch):
+    mock_boto = MagicMock()
+    monkeypatch.setattr('boto3.client', lambda service: mock_boto if service == 'sts' else None)
+    client = StsClient()
+    assert client.sts == mock_boto
+
+def test_caller_identity_property_caches(mock_sts_client):
+    client = StsClient(sts_client=mock_sts_client)
+    # First access triggers get_caller_identity
+    identity1 = client.caller_identity
+    # Second access uses cached value
+    identity2 = client.caller_identity
+    assert identity1 == identity2
+    mock_sts_client.get_caller_identity.assert_called_once()
+
+def test_account_id_property(mock_sts_client):
+    client = StsClient(sts_client=mock_sts_client)
+    assert client.account_id == '123456789012'
+
+def test_get_caller_identity_success(mock_sts_client):
+    client = StsClient(sts_client=mock_sts_client)
+    result = client.get_caller_identity()
+    assert result['Account'] == '123456789012'
+    mock_sts_client.get_caller_identity.assert_called_once()
+
+def test_get_caller_identity_error():
+    mock = MagicMock()
+    mock.get_caller_identity.side_effect = Exception('fail')
+    client = StsClient(sts_client=mock)
+    with pytest.raises(PsenvInternalError) as excinfo:
+        client.get_caller_identity()
+    assert 'Failed to get caller identity' in str(excinfo.value)
+
+def test_raise_if_invalid_account_valid(mock_sts_client):
+    client = StsClient(sts_client=mock_sts_client)
+    # Should not raise
+    client.raise_if_invalid_account('123456789012')
+
+def test_raise_if_invalid_account_invalid(mock_sts_client):
+    mock_sts_client.get_caller_identity.return_value = {'Account': '999999999999'}
+    client = StsClient(sts_client=mock_sts_client)
+    with pytest.raises(PsenvInvalidAccountError) as excinfo:
+        client.raise_if_invalid_account('123456789012')
+    assert 'Invalid AWS account ID' in str(excinfo.value)
+
